792c69b3ef3b09404182fe191b6b0f41921c4567
Kyle Oba <koba@mudphone.com>
2014-03-12 18:37:35 -1000

    Make contributor name search case-insensitive.

--- app/controllers/contribution_search_controller.rb
+++ app/controllers/contribution_search_controller.rb
@@ -1,7 +1,7 @@
 class ContributionSearchController < ApplicationController
   def index
     search_token = params[:search_token]
-    results = Contribution.where("contributor_name like ?", "%#{search_token}%")
+    results = Contribution.where("contributor_name ilike ?", "%#{search_token}%")
 
     respond_to do |format|
       format.json { render json: results }
