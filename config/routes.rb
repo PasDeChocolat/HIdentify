96d6dc893c8324e9c66a33ad827a6b34c7145fe7
Kyle Oba <koba@mudphone.com>
2014-03-29 10:44:34 -1000

    Add groupings index as default route.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ HIdentify::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'groupings#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
